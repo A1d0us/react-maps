9bc6cc58448fdd39accdca4a8542acc11e65d836
a1d0us <diakonenko.vanya@gmail.com>
2024-12-19 20:04:24 +0100

    typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import LocationSearch from "./components/LocationSearch.tsx";
+import Map from "./components/Map.tsx";
+import {Place} from "./api/Place.ts";
+import {useState} from "react";
+
 function App() {
+  const [place, setPlace] = useState<Place | null>(null);
 
   return (
-    <>
-      App
-    </>
+    <div className="h-screen w-screen grid grid-cols-12">
+      <div className="col-span-3 p-2">
+        <LocationSearch onPlaceClick={(p) => setPlace(p)}/>
+      </div>
+      <div className="col-span-9">
+        <Map place={place}/>
+      </div>
+    </div>
   )
 }
 
